af7b1b02092e12749f089d489e2f8f029d81c46e
Ben Hong <ben@bencodezen.io>
2021-10-12 12:43:40 -0400

    feature: simple toldo app

--- pages/todo/index.vue
+++ pages/todo/index.vue
@@ -1,9 +1,37 @@
-<script>
-export default {}
+<script setup>
+import { reactive } from 'vue'
+
+const todoStore = reactive({
+  taskList: [
+    {
+      id: 1,
+      label: 'Ramen'
+    }
+  ],
+  userTaskInput: ''
+})
+
+const addNewTask = () => {
+  todoStore.taskList.push({
+    id: todoStore.taskList.length + 1,
+    label: todoStore.userTaskInput
+  })
+  todoStore.userTaskInput = ''
+}
 </script>
 
 <template>
   <h1>Todo App</h1>
+  <input
+    type="text"
+    v-model="todoStore.userTaskInput"
+    @keyup.enter="addNewTask"
+  />
+  <ul>
+    <li v-for="task in todoStore.taskList" :key="task.id">
+      {{ task.label }}
+    </li>
+  </ul>
 </template>
 
 <style></style>
