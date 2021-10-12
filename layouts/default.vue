a143c78f976c623076771522b122a32d914607b1
Ben Hong <ben@bencodezen.io>
2021-10-12 13:11:08 -0400

    feature: add custom slots for default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <MainNav />
-    <slot />
+    <slot name="header">
+      <h2>My Default header</h2>
+    </slot>
+    <slot name="default" />
+    <slot name="footer">
+      <footer>Copyright {{ new Date().getFullYear() }}</footer>
+    </slot>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <script>
-export default {}
+export default {
+  layout: false
+}
 </script>
 
 <template>
-  <div>
-    <h1>Home Page</h1>
-  </div>
+  <NuxtLayout name="default">
+    <template #default>
+      <p>
+        Ullamco incididunt officia laborum occaecat do enim veniam. Tempor sint
+        officia aliqua et dolore eiusmod non tempor elit qui culpa laboris.
+        Laborum ad labore reprehenderit nisi aliquip ex culpa qui id
+        exercitation consequat laboris nostrud.
+      </p>
+    </template>
+    <template #header><h1>Home Page</h1></template>
+  </NuxtLayout>
 </template>
 
 <style></style>
